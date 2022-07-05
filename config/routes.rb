5cca02ed5fb62ecaaf2bc22b8526d04fc43bd76f
cynthiainga <candyinga67@gmail.com>
2022-07-05 12:15:41 +0200

    Implement the application routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'static/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'static#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1, defaults: { format: :json } do
+      get :hello_world, to: 'helloworld#index'
+    end
+  end
+
+  get '*path', to: 'static#index', constraints: ->(request) do
+    !request.xhr? || request.format.html?
+  end
 end
